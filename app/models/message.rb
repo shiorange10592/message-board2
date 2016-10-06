170b61de00db971ca7df04fae0a1882c71d0e9d1
hideto shiozawa <shiorange10592@yahoo.co.jp>
2016-10-06 02:50:02 +0000

    Kadai2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,9 @@ class Message < ActiveRecord::Base
     # 名前は必須入力かつ２０文字以内
     validates :name , length: { maximum: 20 } , presence: true
     
+    # 年齢は必須入力かつ0以上100以下
+    validates :age ,  numericality: { greater_than_or_equal_to:0, less_than_or_equal_to:100 }, presence: true
+    
     # 内容は必須入力かつ２文字以上３０文字以下
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
     
